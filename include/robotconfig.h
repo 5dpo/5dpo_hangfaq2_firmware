fc40d27821273750a88eec9e936f351b26d28668
Ricardo B. Sousa <sousa.ricardob@outlook.com>
2022-11-08 16:31:24 +0000

    Fix heartbeat frequency

--- include/robotconfig.h
+++ include/robotconfig.h
@@ -56,9 +56,9 @@ const unsigned long kMotCtrlTimeUs = 1000000UL / kMotCtrlFreq;
 const unsigned long kMotCtrlTimeout = 100UL;  //!< watchdog timeout (ms)
 const bool kMotCtrlTimeoutEnable = false;     //!< enable watchdog (true/false)
 
-const unsigned long kMotCtrlLEDOkFreq = 4UL;  //!< heartbeat LED frequency (Hz)
+const unsigned long kMotCtrlLEDOkFreq = 2UL;  //!< heartbeat LED frequency (Hz)
 const unsigned long kMotCtrlLEDOkCount =
-    1000000UL / kMotCtrlLEDOkFreq / kMotCtrlTimeUs;
+    1000000UL / kMotCtrlLEDOkFreq / kMotCtrlTimeUs / 2;
 
 const float kMotVmax = 12;          //!< maximum voltage appliable to motors (V)
 const int16_t kMotPWMmax = 255;     //!< maximum PWM (0..1024)
